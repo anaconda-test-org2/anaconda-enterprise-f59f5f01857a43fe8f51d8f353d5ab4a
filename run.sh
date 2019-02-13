ed3033849936d111c0c2fb6f3897979a17a52a52
anaconda-enterprise <anaconda-enterprise@continuum.io>
2019-02-13 20:04:14 +0000

    Uploaded

---
+++ run.sh
@@ -0,0 +1,7 @@
+set -e
+set -x
+
+npm install --save deck.gl luma.gl
+npm install webpack-dev-server -g
+npm install
+npm start
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+// avoid destructuring for older Node version support
+const resolve = require('path').resolve;
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    app: resolve('./app.js')
+  },
+
+  devtool: 'source-map',
+
+  module: {
+    rules: [{
+      // Compile ES2015 using buble
+      test: /\.js$/,
+      loader: 'buble-loader',
+      include: [resolve('.')],
+      exclude: [/node_modules/],
+      options: {
+        objectAssign: 'Object.assign'
+      }
+    }]
+  },
+
+  resolve: {
+    alias: {
+      // From mapbox-gl-js README. Required for non-browserify bundlers (e.g. webpack):
+      'mapbox-gl$': resolve('./node_modules/mapbox-gl/dist/mapbox-gl.js')
+    }
+  },
+  devServer: {
+   host: '0.0.0.0',
+   disableHostCheck: true
+ },
+
+  // Optional: Enables reading mapbox token from environment variable
+  plugins: [
+    new webpack.EnvironmentPlugin(['MAPBOX_ACCESS_TOKEN', 'MapboxAccessToken'])
+  ]
+};
